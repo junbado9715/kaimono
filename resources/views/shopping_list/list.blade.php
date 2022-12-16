c2edfad427cd7b3b04f229ba0833e527afae6c3a
junmorita <jub.bado.9715@icloud.com>
2022-12-16 18:03:59 +0000

    課題提出_3回目

--- resources/views/shopping_list/list.blade.php
+++ resources/views/shopping_list/list.blade.php
@@ -44,6 +44,7 @@
                 @csrf
                 <button onclick='return confirm("この「買うもの」を「完了」にします。よろしいですか？");'>完了</button>
             </form>
+            <td>&emsp;</td>
             <td><form action="{{ route('delete', ['shopping_list_id' => $shopping_list->id]) }}" method="post">
                 @csrf
                 @method("DELETE")
